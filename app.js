c7d0c520922e51ef5126f8caeab153a0fc03a8cc
David Shlomo Hestrin <davidshlomohestrin@davids-mbp.attlocal.net>
2017-06-07 13:38:33 -0700

    Changed some text and started styling the simple app.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 $("#submit").on("click", function(){
-  var $input = $("#movie_title");
+  var $input = $("#string");
   if ($input.val().length > 0){
-  firebase.database().ref().child("movies").push($input.val());
+  firebase.database().ref().child("strings").push($input.val());
   $input.val("");
   }
 });
 
 $(document).ready(function(){
-  var moviesReference = firebase.database().ref("movies");
+  var moviesReference = firebase.database().ref("strings");
   moviesReference.on("child_added", function(data){
-    $("#movie_list").append("<li>" + data.val() + "</li>")
+    $("#strings").append("<li>" + data.val() + "</li>")
   });
 });
