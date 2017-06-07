2cf236957ad6edcd7c1f0748dbc1dc0787c8f022
David Shlomo Hestrin <davidshlomohestrin@davids-mbp.attlocal.net>
2017-06-07 13:29:17 -0700

    Simplified the simple app.

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 $("#submit").on("click", function(){
-  var $input = $("#book_title");
+  var $input = $("#movie_title");
   if ($input.val().length > 0){
-  firebase.database().ref().child("books").child(0).set($input.val());
+  firebase.database().ref().child("movies").push($input.val());
   $input.val("");
-  setTimeout(function(){
-    $input.attr("placeholder","Submit another book NOW");
-
-  }, 500)
-} else{
-  alert("put a book title!")
-};
+  }
 });
 
 $(document).ready(function(){
-  var booksReference = firebase.database().ref("books");
-  booksReference.on("child_added", function(data){
-    $("#book_list").append("<li>" + data.val() + "</li>")
+  var moviesReference = firebase.database().ref("movies");
+  moviesReference.on("child_added", function(data){
+    $("#movie_list").append("<li>" + data.val() + "</li>")
   });
 });
